ba4e44e9a048bfe503592b9a47335e0e4faadbab
Jacob Devera <jacob.devera12@gmail.com>
2017-10-08 17:28:14 -0700

    added animations

--- js/main.js
+++ js/main.js
@@ -1,27 +1,29 @@
-(function($) {
-  "use strict"; // Start of use strict
+"use strict";
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 48)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
+// Smooth scrolling using jQuery easing
+$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    if (target.length) {
+      $('html, body').animate({
+        scrollTop: (target.offset().top - 48)
+      }, 1000, "easeInOutExpo");
+      return false;
     }
-  });
+  }
+});
 
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-shrink");
-    } else {
-      $("#mainNav").removeClass("navbar-shrink");
-    }
-  });
-})(jQuery); // End of use strict
+// Collapse the navbar when page is scrolled
+$(window).scroll(function () {
+  if ($("#mainNav").offset().top > 100) {
+    $("#mainNav").addClass("navbar-shrink");
+  } else {
+    $("#mainNav").removeClass("navbar-shrink");
+  }
+});
 
+
+$(window).scroll(function () {
+  $("#intro-scroll").css("opacity", 1 - $(window).scrollTop() / ($(window).height() / 1.5));
+});
