0c32bf2f6c5e0ec29273f50972e993ba6d5e5b99
Jacob Devera <jacob.devera12@gmail.com>
2017-10-13 21:34:41 -0700

    Fixed flexboxes

--- js/main.js
+++ js/main.js
@@ -15,15 +15,11 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
 });
 
 // Collapse the navbar when page is scrolled
-$(window).scroll(function () {
+$(window).scroll( () => {
   if ($("#mainNav").offset().top > 100) {
     $("#mainNav").addClass("navbar-shrink");
   } else {
     $("#mainNav").removeClass("navbar-shrink");
   }
-});
-
-
-$(window).scroll(function () {
   $("#intro-scroll").css("opacity", 1 - $(window).scrollTop() / ($(window).height() / 1.5));
 });
