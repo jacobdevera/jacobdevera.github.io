bf9ab69a5561a755e67bc31347526a1782938736
Jacob Devera <jacob.devera12@gmail.com>
2017-11-03 18:48:28 -0700

    adjust scrolling of link, move projects section above about

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
     target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
     if (target.length) {
       $('html, body').animate({
-        scrollTop: (target.offset().top - 48)
+        scrollTop: (target.offset().top - 68)
       }, 1000, "easeInOutExpo");
       return false;
     }
