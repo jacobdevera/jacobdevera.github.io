ff6f329419c300c0540bccc3e37c8cb8ccfbbf96
Jacob Devera <jacob.devera12@gmail.com>
2022-01-06 21:57:44 -0800

    styling adjustments

--- js/main.js
+++ js/main.js
@@ -1,19 +1,5 @@
 "use strict";
 
-// Smooth scrolling using jQuery easing
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash);
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: (target.offset().top - 68)
-      }, 1000, "easeInOutExpo");
-      return false;
-    }
-  }
-});
-
 // Collapse the navbar when page is scrolled
 $(window).scroll( () => {
   if ($("#mainNav").offset().top > 100) {
